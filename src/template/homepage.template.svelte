8bef6b85533372b7440ce5b595a2b5a572059d1a
Abhishek <Badhanabhishek3@gmail.com>
2023-03-29 06:34:56 +0000

    Add head title and favicon

--- src/template/homepage.template.svelte
+++ src/template/homepage.template.svelte
@@ -1,12 +1,26 @@
-<div
-	class="bg-neutral-600 text-white flex flex-col pt-60 h-screen w-screen items-center gap-5"
->
+<script>
+	console.log(`
+ __  |__  |__  o   __ |__   ___  |__, 
+(__( |__) |  ) | __)  |  ) (__/_ |  \\ 
+	`);
+	console.info("contact here: badhanabhishek3@gmail.com")
+</script>
+
+<head>
+	<title>abhishek</title>
+</head>
+
+
+<div class="bg-neutral-600 text-white flex flex-col pt-60 h-screen w-screen items-center gap-5">
 	<div class="text-xl">
-		I create software @ <a class="link" target="_blank" href="https://www.cuilsoft.com/">cuilsoft</a>
+		I create software @ <a class="link" target="_blank" href="https://www.cuilsoft.com/">cuilsoft</a
+		>
 	</div>
 	<div class="flex gap-2">
 		<a target="_blank" class="link" href="https://twitter.com/beizti">Twitter</a>
 		<a target="_blank" class="link" href="https://github.com/Badhan-abhishek">Github</a>
-		<a target="_blank" class="link" href="https://www.linkedin.com/in/abhishek-4b3b49198/">LinkedIn</a>
+		<a target="_blank" class="link" href="https://www.linkedin.com/in/abhishek-4b3b49198/"
+			>LinkedIn</a
+		>
 	</div>
 </div>
