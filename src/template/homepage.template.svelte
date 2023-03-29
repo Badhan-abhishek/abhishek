f91e50135b58adaf21bd1baaab3dc4eccf46ae69
Abhishek <Badhanabhishek3@gmail.com>
2023-03-29 06:20:41 +0000

    Update

--- src/template/homepage.template.svelte
+++ src/template/homepage.template.svelte
@@ -1,8 +1,12 @@
-<script lang="ts">
-	import Header from '../components/atoms/header.atom.svelte';
-	import Float from '../components/atoms/float.atom.svelte';
-</script>
-
-<Float />
-
-<Header />
+<div
+	class="bg-neutral-600 text-white flex flex-col pt-60 h-screen w-screen items-center gap-5"
+>
+	<div class="text-xl">
+		I create software @ <a class="link" target="_blank" href="https://www.cuilsoft.com/">cuilsoft</a>
+	</div>
+	<div class="flex gap-2">
+		<a target="_blank" class="link" href="https://twitter.com/beizti">Twitter</a>
+		<a target="_blank" class="link" href="https://github.com/Badhan-abhishek">Github</a>
+		<a target="_blank" class="link" href="https://www.linkedin.com/in/abhishek-4b3b49198/">LinkedIn</a>
+	</div>
+</div>
