b15b36e14f3c2642d6ff01bfbc8b30f87c8660d5
rm-rf-main <rajat@kicklaunch.com>
2023-05-15 18:46:42 +0530

    Add showcase

---
+++ src/components/molecules/showcase.molecule.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	const showCaseData = [
+		{
+			name: 'Button',
+			url: 'https://badhan-abhishek.github.io/running-border-btn/index',
+			image: '/button.png'
+		}
+	];
+</script>
+
+<div class="w-2/3">
+	<div class="text-2xl pb-3">Showcase</div>
+	<div class="mx-auto">
+		<div class="grid grid-cols-2 gap-20">
+			{#each showCaseData as showcase}
+				<a
+					href={showcase.url}
+					class="relative group border rounded-xl overflow-hidden shadow-xl shadow-black"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<div
+						class="absolute hidden group-hover:flex h-full w-full bg-[#ffffff99] bg-blur-lg text-black"
+					>
+						<span class="grid place-items-center z-10 w-full"> {showcase.name} </span>
+					</div>
+					<img src={showcase.image} alt="Running Border Button" />
+				</a>
+			{/each}
+		</div>
+	</div>
+</div>
